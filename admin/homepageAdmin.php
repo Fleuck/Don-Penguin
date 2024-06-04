12d414bc57b79d786f5e94ae606adbcac9179208
Bernardo Jakubiak <bernardo.jakubiak@gmail.com>
2024-06-03 21:06:52 -0300

    session timeout, tabelas adm, reformulação

--- admin/homepageAdmin.php
+++ admin/homepageAdmin.php
@@ -10,77 +10,151 @@ if(isset($_SESSION['userName'])) {
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
+    <style>
+    .texto-branco {
+        color: white;
+    }
+    </style>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./home.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <title>Don Penguin</title>
 </head>
-
-<body>
-    <div class="all" id="all">
-        <header>
-            <div class="org">
-                <img class="logo" src="../midia/penguin.jpg" alt="logo">
-                <nav>
-                    <ul class="barra">
-                        <a class="menu" href=""><li><?php echo "Bem-vindo, $username!"; ?></li></a>
-                    </ul>
-                </nav>
-                <div class = "toggle">
-                    <i class="fa-solid fa-bars"></i>
-                </div>
-                
-            </div>
-        </header> 
-
-        <a href="../layoutTelaFilme/layout.php"><div class="container" id="movie-container"></div></a>
-        <div class="searchBar" id="searchBar">
-            <form>
-                <input type="text" id="nome" name="nome" placeholder="O que vai assisitr ?"><br>
-            </form>          
-        </div>
-        
+<body style="background-color: #1c1c1c">
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+        <img src="../midia/penguin.jpg" alt="Bootstrap" width="30" height="24"><?php echo "Bem-vindo, $username!"; ?>
+        </a>
     </div>
-    <table class="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nome do Usuário</th>
-      <th scope="col">E-mail</th>
-    </tr>
-  </thead>
-  <tbody>
-  <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "projetoDonPenguin";
-    
-    $pdo = new PDO('mysql:host=localhost;dbname=projetoDonPenguin', $username, $password);
+    </nav>
+    <div class="container text-center">
+  <div class="row texto-branco">
+    <div class="col">
+    <div class="table-responsive">
+        <table class="table table-dark table-sm">
+        <thead style="padding-">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome do Usuário</th>
+            <th scope="col">E-mail</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "projetoDonPenguin";
 
-    try {
-        $consulta = $pdo->query("SELECT nome_usuario, email FROM usuario;");
+        $pdo = new PDO('mysql:host=127.0.0.1:3307;dbname=projetoDonPenguin', $username, $password);
 
-        while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
-        echo "<tr>";
-        echo "<th scope='row'> </th>";
-        echo "<td>{$linha['nome_usuario']}</td>";
-        echo "<td>{$linha['email']}</td>";
-        echo "</tr>";
+        try {
+            $consulta = $pdo->query("SELECT nome_usuario, email FROM usuario;");
+
+            while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr>";
+            echo "<th scope='row'> </th>";
+            echo "<td>{$linha['nome_usuario']}</td>";
+            echo "<td>{$linha['email']}</td>";
+            echo "</tr>";
+            }
+        } catch (PDOException $e) {
+            echo 'Erro: ' . $e->getMessage();
         }
-    } catch (PDOException $e) {
-        echo 'Erro: ' . $e->getMessage();
-    }
-    ?>
-  </tbody>
-</table>
+        ?>
+        </tbody>
+        </table>
+        </div>
+    </div></div>
+    <div class="row texto-branco">
+    <div class="col">
+    <div class="table-responsive">
+        <table class="table table-dark table-sm">
+            <thead style="padding-">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">URL Filme</th>
+                <th scope="col">capa filme</th>
+                <th scope="col">nome filme</th>
+                <th scope="col">nota filme</th>
+                <th scope="col">lançamento</th>
+                <th scope="col">genero</th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $database = "projetoDonPenguin";
+
+            $pdo = new PDO('mysql:host=127.0.0.1:3307;dbname=projetoDonPenguin', $username, $password);
+
+            try {
+                $consulta = $pdo->query("SELECT url_filme, sinopse, capa_filme, nome_filme, nota_filme, lancamento, genero_filme FROM filme;");
+
+                while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+                echo "<tr>";
+                echo "<th scope='row'> </th>";
+                echo "<td>{$linha['url_filme']}</td>";
+                echo "<td>{$linha['capa_filme']}</td>";
+                echo "<td>{$linha['nome_filme']}</td>";
+                echo "<td>{$linha['nota_filme']}</td>";
+                echo "<td>{$linha['lancamento']}</td>";
+                echo "<td>{$linha['genero_filme']}</td>";
+                echo "</tr>";
+                }
+            } catch (PDOException $e) {
+                echo 'Erro: ' . $e->getMessage();
+            }
+            ?>
+        </tbody>
+        </table>
+        </div>
+    </div>
+    </div>
+    <div class="row texto-branco">
+        <div class="col">
+        <div class="table-responsive">
+        <table class="table table-dark table-sm">
+        <thead style="padding-">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome do Usuário</th>
+            <th scope="col">E-mail</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $database = "projetoDonPenguin";
+
+        $pdo = new PDO('mysql:host=127.0.0.1:3307;dbname=projetoDonPenguin', $username, $password);
+
+        try {
+            $consulta = $pdo->query("SELECT nome_ator, descricao_ator FROM ator;");
+
+            while ($linha = $consulta->fetch(PDO::FETCH_ASSOC)) {
+            echo "<tr>";
+            echo "<th scope='row'> </th>";
+            echo "<td>{$linha['nome_ator']}</td>";
+            echo "<td>{$linha['descricao_ator']}</td>";
+            echo "</tr>";
+            }
+        } catch (PDOException $e) {
+            echo 'Erro: ' . $e->getMessage();
+        }
+        ?>
+        </tbody>
+        </table>
+        </div>
+        </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
--- verificar_login.php
+++ verificar_login.php
@@ -1,8 +1,22 @@
 <?php
 session_start();
 
+$timeout = 1800;
+
 if (!isset($_SESSION['userID'])) {
     header("Location: ../TelaLogin/telalogin.html");
     exit();
 }
+
+if (isset($_SESSION['last_activity']) && (time() - $_SESSION['last_activity']) > $timeout) {
+    session_unset();
+    session_destroy();
+    echo "<script>";
+    echo "alert('Sua sessão expirou!');";
+    echo "</script>";
+    header("Location: ../TelaLogin/telalogin.html");
+    exit();
+}
+
+$_SESSION['last_activity'] = time();
 ?>
